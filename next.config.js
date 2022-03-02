e2d306f7bf81b695ac6a199e7226eada3332ac74
Davit Technologies <david@davit.ie>
2022-03-02 02:07:03 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,11 +1,7 @@
-module.exports = (phase, { defaultConfig }) => {
-  /**
-   * @type {import('next').NextConfig}
-   */
-  const nextConfig = {
-    /* config options here */
-    basePath: '/nextsite-0.1.0',
-  }
-  return nextConfig
-}
+//
+const basePath = process.env.NODE_ENV === 'production' ? '/nextsite-0.1.0' : '';
 
+module.exports = {
+  basePath,
+  assetPrefix: `${basePath}/`
+};
