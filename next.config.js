0348bac6bee69589715826027ce7e056b48c510e
Davit Technologies <david@davit.ie>
2022-03-02 01:01:18 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,13 +1,3 @@
 module.exports = {
   basePath: '/nextsite-0.1.0',
-  trailingSlash: false,
-  exportPathMap: async function(
-    defaultPathMap,
-    {dev, dir, outDir, distDir, buildId}
-  ) {
-    return {
-      "/": {page: "/"},
-      "/posts": {page: "/posts"}
-    };
-  }
-};
+}
